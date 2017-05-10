1a1b15b80c2bbb2ac4d2313dfe08adbae660ccb5
Nikolay Bogdanov <nbogdanov@mirantis.com>
2017-05-10 16:05:29 +0400

    Adds loop test

--- bot.js
+++ bot.js
@@ -2,22 +2,38 @@ var token = process.env.TOKEN;
 
 var Bot = require('node-telegram-bot-api');
 var bot;
+var fetcher;
 
-if(process.env.NODE_ENV === 'production') {
+var syncer = function() {
+  console.log('Loop');
+  fetcher = setTimeout(syncer, 2000);
+};
+
+if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token);
-}
-else {
-  bot = new Bot(token, { polling: true });
-}
+  bot.setWebHook(process.env.HEROKU_URL + bot.token, {
+  	certificate: 'crt.pem'
+  });
+} else {
+  bot = new Bot(token, {
+    polling: true 
+  });
+};
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.onText(/^/, function (msg) {
   var name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
-    // reply sent!
-  });
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(
+    function () {
+    }
+  );
 });
+
+bot.on('webhook_error', function (error) {
+  console.log(error.code);
+});
+
+syncer();
 
 module.exports = bot;
