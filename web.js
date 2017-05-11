023bce83109e72b09294ae9d59807350e16bce42
Nikolay Bogdanov <nbogdanov@mirantis.com>
2017-05-11 11:01:57 +0400

    Post request body logging

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/push', function (req, res) {
-	console.log(req.body);
+	console.log('POST received:', req.body);
 });
 
 var server = app.listen(process.env.PORT, "0.0.0.0", function () {
