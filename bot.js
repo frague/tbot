c074e0607026e7dd575dbbc888679a8c4f876681
Nikolay Bogdanov <nbogdanov@mirantis.com>
2017-05-19 22:11:48 +0400

    Removes cert

--- bot.js
+++ bot.js
@@ -61,9 +61,7 @@ function KickActions(message, bot, isKick) {
 
 if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token, {
-  	certificate: 'crt.pem'
-  });
+  bot.setWebHook(process.env.HEROKU_URL + bot.token, {});
 } else {
   bot = new Bot(token, {
     polling: true 
